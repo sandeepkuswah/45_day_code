4b93fdf10902daaa5695bb447fa96b520fd9113a
Sandeep Singh kushwah <sandeepkushwah6162@gmail.com>
2024-10-13 09:41:14 +0530

    Add files via upload

---
+++ Day_13NOTEBOOK.py
@@ -0,0 +1,15 @@
+# cook your dish here
+# Input number of test cases
+T = int(input())
+
+# Iterate over each test case
+for _ in range(T):
+    # Input weight of pulp (N kg)
+    N = int(input())
+    
+    # Calculate the number of notebooks
+    notebooks = N * 10
+    
+    # Output the result for this test case
+    print(notebooks)
+    
